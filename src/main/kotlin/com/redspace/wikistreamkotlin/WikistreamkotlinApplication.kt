7407680ece83aea25a70f0e9cba8dbb82c446857
Iryna Oliinyk <iryna.oliinyk@redspace.com>
2026-04-15 12:47:03 -0300

    init commit

---
+++ src/main/kotlin/com/redspace/wikistreamkotlin/WikistreamkotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.redspace.wikistreamkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WikistreamkotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<WikistreamkotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/redspace/wikistreamkotlin/WikistreamkotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.redspace.wikistreamkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WikistreamkotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
